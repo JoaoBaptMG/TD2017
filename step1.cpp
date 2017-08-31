ad43443dadcb92e6863daf62502e47d10a2f0641
João Baptista de Paula e Silva <jbaptistapsilva@yahoo.com.br>
2017-08-30 22:06:15 -0300

    Add important structural changes to project

--- step1.cpp
+++ step1.cpp
@@ -2,6 +2,9 @@
 
 ExpPtr convertIntuitionistic(ExpPtr tree)
 {
-    tree = std::make_shared<ExpTree>(ExpTree::Type::Negation, std::make_shared<ExpTree>(ExpTree::Type::Negation, tree));
+    if (tree->getType() == ExpTree::Type::Root)
+        tree->setLeftNode(convertIntuitionistic(tree->getLeftNode()));
+    else tree = std::make_shared<ExpTree>(ExpTree::Type::Negation, std::make_shared<ExpTree>(ExpTree::Type::Negation, tree));
+    
     return tree;
 }
